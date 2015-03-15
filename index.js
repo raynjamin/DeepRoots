2a3f0c4374b3964a0b0007916aac6f69b0faa9a4
Ben Sterrett <raynjamin@gmail.com>
2015-03-15 14:43:25 -0400

    Fixing port number for webfaction, adding contact/about pages, fixing some links

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
+app.get('/about', function (req, res) {
+	res.render('about');
+})
+
+app.get('/contact', function (req, res) {
+	res.render('contact');
+})
+
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(12245, function () {
 	// var host = server.address().address;
 	// var port = server.address().port;
 });
