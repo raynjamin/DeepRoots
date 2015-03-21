c036d639f66003de86b424979e4b920ff4269961
Ben Sterrett <raynjamin@gmail.com>
2015-03-20 22:59:39 -0400

    Adding in form logic, validation, e-mails.

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mailer = require('nodemailer');
 var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// create reusable transporter object using SMTP transport
+var transporter = mailer.createTransport({
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.DEEPROOTS_EMAIL_USER,
+        pass: process.env.DEEPROOTS_EMAIL_PASSWORD
+    },
+    host: 'smtp.webfaction.com'
+});
+
+function mailContact(name, email, phone, serviceType, message) {
+  transporter.sendMail({
+    from: 'billy.arrington@deeprootscarolinas.com',
+    to: 'billy.arrington@deeprootscarolinas.com',
+    subject: 'New Website Inquiry',
+    text: ['Service Inquiry from ',
+            name,
+            ' at email address: ',
+            email,
+            ' and phone number: ',
+            phone,
+            ' looking for help with ',
+            serviceType,
+            '. They have additionally included the following message: ',
+            message].join("")
+  });
+}
+
+
 app.get('/', function (req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 app.get('/about', function (req, res) {
-	res.render('about');
+  res.render('about');
 })
 
 app.get('/contact', function (req, res) {
-	res.render('contact');
+  res.render('contact');
 });
 
 app.post('/contact', function (req, res) {
-
+  mailContact(req.body.Name, req.body.Email, req.body.PhoneNumber, req.body.RequestType, req.body.Message);
+  res.render('contact');
 });
 
 app.use(express.static('public'));
 
 var server = app.listen(12245, function () {
-	// var host = server.address().address;
-	// var port = server.address().port;
 });
